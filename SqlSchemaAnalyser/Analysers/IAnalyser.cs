f89c7fdcd91e8d98662d0aa23b3679b2623631c3
Robin3880 <robinkonrad101@gmail.com>
2026-08-23 17:51:51 +0100

    remove access modifier from IAnalyser interface method

--- SqlSchemaAnalyser/Analysers/IAnalyser.cs
+++ SqlSchemaAnalyser/Analysers/IAnalyser.cs
@@ -4,6 +4,7 @@ using Models;
 
 public interface IAnalyser
     {
-        public Task<(List<Finding> Findings, LlmUsageData Usage)> AnalyseAsync(string schema); //analyse schema and return findings
-
+        
+        Task<(List<Finding> Findings, LlmUsageData Usage)> AnalyseAsync(string schema); //analyse schema and return findings
+        
     }
