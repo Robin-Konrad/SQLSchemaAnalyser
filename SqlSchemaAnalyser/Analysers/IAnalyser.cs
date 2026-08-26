603ae2a2934b85c727201a665e5171a4fe3fd1fb
Robin3880 <robinkonrad101@gmail.com>
2026-08-26 23:48:45 +0100

    add Category back to IAnalyser

--- SqlSchemaAnalyser/Analysers/IAnalyser.cs
+++ SqlSchemaAnalyser/Analysers/IAnalyser.cs
@@ -4,7 +4,7 @@ using Models;
 
 public interface IAnalyser
     {
-
+        string Category { get; }
         Task<(List<Finding> Findings, LlmUsageData Usage)> AnalyseAsync(List<string> statements); //analyse schema and return findings
         
     }
